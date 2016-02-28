404992393a387b2566d917d03c3bbc173fc9af48
Kenichiro Arai <ken.arai.1988@gmail.com>
2016-02-28 14:27:16 -0700

    ++

--- scroll_config.js
+++ scroll_config.js
@@ -1,8 +1,6 @@
-
     (function($) {
         var elem = $.jInvertScroll(['.scroll'],        // an array containing the selector(s) for the elements you want to animate
             {
-            height: 9000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
             onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
                 console.log(percent);
             }
